c0f454b14745a2202e464c23ac79576f8e0defb1
ricardopiloto <ac.ricardosobral@gmail.com>
2016-10-15 12:01:33 -0300

    Initial commit

--- comsetup.log/app/src/main/java/com/example/ponti/izirpg/MainActivity.java
+++ comsetup.log/app/src/main/java/com/example/ponti/izirpg/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String CharName = "William Pendragon";
 
     // Example of a call to a native method
     TextView tv = (TextView) findViewById(R.id.sample_text);
