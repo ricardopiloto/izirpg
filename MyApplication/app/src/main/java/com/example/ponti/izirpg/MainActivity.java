2ad0954cdb92ea18a64b45b821273ab092f4da8b
Ricardo <ac.ricardosobral@gmail.com>
2016-10-22 13:47:03 -0200

    asd

--- MyApplication/app/src/main/java/com/example/ponti/izirpg/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/ponti/izirpg/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.ponti.izirpg;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CharList extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity {
+    String url = "http://localhost/android/webservices/json_code.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_char_list);
+        setContentView(R.layout.activity_main);
     }
 }
