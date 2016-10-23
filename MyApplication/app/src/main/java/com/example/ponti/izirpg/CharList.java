ce3462297be729e316e44954ade281444efcc6c6
Ricardo <ac.ricardosobral@gmail.com>
2016-10-23 10:59:18 -0200

    Created a list to hold the json parsed information

--- MyApplication/app/src/main/java/com/example/ponti/izirpg/CharList.java
+++ MyApplication/app/src/main/java/com/example/ponti/izirpg/CharList.java
@@ -1,20 +1,19 @@
 package com.example.ponti.izirpg;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
 
-/**
- * Created by ponti on 22/10/2016.
- */
+public class CharList extends AppCompatActivity {
 
-public class CharList extends AppCompatActivity{
+    String url = "http://localhost/android/webservices/get_chars.php";
 
-    EditText charNames;
+    private static final String TAG_RESULTS = "result";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_ADD ="player_name";
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_char_list);
     }
